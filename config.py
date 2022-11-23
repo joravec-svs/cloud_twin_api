eaed0d2890e1b9a17b5963db9775838d75d25d40
joravec-svs <joravec@svsfem.cz>
2022-11-23 15:52:33 +0100

    database_url

--- config.py
+++ config.py
@@ -4,10 +4,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     INSTANCE_NAME = "europe-central2:devel-sql"
     PROJECT_ID = "devel-12345"
-    PUBLIC_IP_ADDRESS = "/cloudsql/devel-12345:europe-central2:devel-sql"  #"127.0.0.1:3306"
+    PUBLIC_IP_ADDRESS = "127.0.0.1:3306"
+    SOCKET_PATH = "/cloudsql/devel-12345:europe-central2:devel-sql"
     PASSWORD = "Bonvolu53"
     DBNAME = "Data"
 
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'svs-fem-secret-key'
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}"
+    # Equivalent URL:
+    # f"mysql+pymysql://root:{PASSWORD}@/{DBNAME}?unix_socket={SOCKET_PATH}"
+
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:{PASSWORD}@/{DBNAME}?unix_socket={SOCKET_PATH}" #f"mysql+pymysql://root:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
