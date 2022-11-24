3e343bca8ceb78f388c798ce0c626f2614b3881e
joravec-svs <joravec@svsfem.cz>
2022-11-24 15:25:50 +0100

    config.py update

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from config import Config
+from config import DevConfig,ProdConfig
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
@@ -9,14 +9,14 @@ migrate = Migrate()
 login = LoginManager()
 login.login_view = 'auth.login'
 
-def create_app(config_class=Config):
+def create_app(config_class=DevConfig):
     app = Flask(__name__)
     app.config.from_object(config_class)
-    
+
     db.init_app(app)
     migrate.init_app(app, db)
     login.init_app(app)
- 
+
     from app.api import bp as api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
 
--- config.py
+++ config.py
@@ -1,17 +1,31 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-class Config(object):
-    INSTANCE_NAME = "europe-central2:devel-sql"
-    PROJECT_ID = "devel-12345"
-    PUBLIC_IP_ADDRESS = "127.0.0.1:3306"
-    SOCKET_PATH = "/cloudsql/devel-12345:europe-central2:devel-sql"
-    PASSWORD = "Bonvolu53"
-    DBNAME = "Data"
-
+class Config():
+    """Base config."""
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'svs-fem-secret-key'
-    # Equivalent URL:
-    # f"mysql+pymysql://root:{PASSWORD}@/{DBNAME}?unix_socket={SOCKET_PATH}"
-
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:{PASSWORD}@/{DBNAME}?unix_socket={SOCKET_PATH}" #f"mysql+pymysql://root:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}"
+    STATIC_FOLDER = 'static'
+    TEMPLATES_FOLDER = 'templates'
+    #SOCKET_PATH = "/cloudsql/devel-12345:europe-central2:devel-sql" 
+    
+class ProdConfig(Config):
+    FLASK_DEBUG = False
+    TESTING = False
+    INSTANCE_NAME = os.environ.get('INSTANCE_NAME')
+    PROJECT_ID = os.environ.get('PROJECT_ID')
+    DB_PASS = os.environ.get('PASSWORD')
+    DB_USER = os.environ.get('DB_USER')
+    DBNAME = os.environ.get('DBNAME')
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASS}@/{DBNAME}?unix_socket=/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+class DevConfig(Config):
+    FLASK_DEBUG = True
+    TESTING = True
+    
+    PUBLIC_IP_ADDRESS = "127.0.0.1:3306"
+    DB_PASS = os.environ.get('PASSWORD')
+    DB_USER = os.environ.get('DB_USER')
+    DBNAME = os.environ.get('DBNAME')
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{PUBLIC_IP_ADDRESS}/{DBNAME}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
